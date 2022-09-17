d2465f6460f9970ab2319ebe7b753d787d0ee8c6
Nicho Del Moral <ndelmoral13@gmail.com>
2022-09-17 01:41:39 -0400

    initial app setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card.js';
+import Header from './components/Header.js';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +24,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <Card/>
     </div>
+
+    </Router>
+    
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import './Card.css';
+
+function Card() {
+    return (
+        <div class="card-container">
+            <h2>AWS Prep</h2>
+            <div>
+                - Review data structures
+            </div>
+            <div>
+                - Review basic algorithms
+            </div>
+            <div>
+                - Show example of array operations and document the runtimes for operations(insertion/deletion) here
+            </div>
+            <div>
+                - Show example of tree traversal here and quick sort
+            </div>
+            <div>
+                - Choose a design architecture for frontend application and explain design choice
+            </div>
+        </div>
+    );
+}
+
+export default Card;
