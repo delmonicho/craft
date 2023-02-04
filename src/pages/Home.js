bd22161cb1b379a95cbac0937bd6136695785e97
Nicho Delmo <ndelmoral13@gmail.com>
2023-02-03 23:02:00 -0500

    style changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,14 @@
 import Card from "../components/Card";
 import Header from "../components/Header";
+import "./Home.css";
 
 export default function Home() {
 
     return (
-        <div>
+        <div class="home-container">
             <Header />
-            <Card title="interview prep"/>
-            <div>
-                - Review data structures
-            </div>
-            <div>
-                - Review basic algorithms
-            </div>
-            <div>
-                - Show example of array operations and document the runtimes for operations(insertion/deletion) here
-            </div>
-            <div>
-                - Show example of tree traversal here and quick sort
-            </div>
-            <div>
-                - Choose a design architecture for frontend application and explain design choice
-            </div>
+            <Card title="ohcin touba"/>
+            <Card title="portfolio"/>
         </div>
     );
 }
